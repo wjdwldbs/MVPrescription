eabf3e7e0b23b6c1bdfbb686f6ba67532d37072b
SassySpaceMan <51337432+SassySpaceMan@users.noreply.github.com>
2019-11-20 12:01:14 -0800

    [style] changed styling on tab bar

--- my-new-project/components/TabBarIcon.js
+++ my-new-project/components/TabBarIcon.js
@@ -7,9 +7,9 @@ export default function TabBarIcon(props) {
   return (
     <Ionicons
       name={props.name}
-      size={26}
+      size={50}
       style={{ marginBottom: -3 }}
-      color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+      color={props.focused ? '#00bbee' : '#ccffff'}
     />
   );
 }
