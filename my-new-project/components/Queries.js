3a52bcedb971ffb8488a0e14069dfeb64db96d68
Daylan <daylanberry@gmail.com>
2019-11-20 09:16:17 -0800

    api queries for image, side effects, patient info, generic name, brand name

--- my-new-project/components/Queries.js
+++ my-new-project/components/Queries.js
@@ -20,41 +20,62 @@ export default class Queries extends React.Component {
     super(props)
 
     this.state = {
+      image: '',
+      name: '',
+      generic: '',
       query: '',
       medication: '',
       strength: '',
       direction: '',
       note: '',
-      sideEffect: ''
+      sideEffect: '',
+      patientInfo: ''
     };
 
     this.getMedication = this.getMedication.bind(this)
+    this.getImage = this.getImage.bind(this)
+    //this.addMedication = this.addMedication.bind(this)
   }
 
   getMedication(query) {
     axios.get(`https://api.fda.gov/drug/label.json?search=description:${query}&limit=1`)
     .then(res => {
       this.setState({
-        medication: res.data
+        //medication: res.data,
+        sideEffect: res.data.results[0].adverse_reactions[0],
+        patientInfo: res.data.results[0].information_for_patients[0],
+        name: query
       })
     })
+    .then(() => this.getImage(query))
     .catch(err => console.log(err))
   }
 
-  addMedication() {
-    axios.post('/drug', {
-      name: this.state.query,
-      strength: this.state.strength,
-      direction: this.state.direction,
-      note: this.state.note,
-      sideEffects: this.state.sideEffect
+  getImage(drugQuery) {
+    axios.get(`https://rximage.nlm.nih.gov/api/rximage/1/rxnav?name=${drugQuery}&rLimit=1`)
+    .then(res => {
+      this.setState({
+        image: res.data.nlmRxImages[0].imageUrl,
+        generic: res.data.nlmRxImages[0].name
+      })
     })
   }
+
+  // addMedication() {
+  //   axios.post('/drug', {
+  //     name: this.state.query,
+  //     generic: this.state.generic
+  //     strength: this.state.strength,
+  //     direction: this.state.direction,
+  //     note: this.state.note,
+  //     sideEffects: this.state.sideEffect
+  //   })
+  // }
 
 
 
   render() {
-    var dosage = [];
+
     return (
     <View>
       <Text>Add a Medication</Text>
@@ -80,9 +101,8 @@ export default class Queries extends React.Component {
         onChangeText={(text) => this.setState({note: text})}
         />
   <Button onPress={() => this.getMedication(this.state.query)} title="click"/>
-  <Button onPress={() => console.log(this.state)} title="click"/>
+  <Button onPress={() => console.log(this.state.sideEffect)} title="click"/>
     </View>
     );
   }
-
 }
