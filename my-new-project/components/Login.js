28d0b8a764d761bb1c6016af59708b67ebf58e6b
SassySpaceMan <51337432+SassySpaceMan@users.noreply.github.com>
2019-11-21 11:29:29 -0800

    [style] styled login page

--- my-new-project/components/Login.js
+++ my-new-project/components/Login.js
@@ -1,7 +1,38 @@
 import React from 'react';
-import { Modal, View, Text, TextInput, TouchableHighlight } from 'react-native';
+import { Modal, View, Text, TextInput, TouchableHighlight, StyleSheet } from 'react-native';
 import axios from 'axios';
-
+const styles =StyleSheet.create({
+    text:{
+      fontSize:25,
+      textDecorationLine: 'underline',
+      paddingLeft:10
+    },
+    smallText:{
+      fontSize:20,
+      fontStyle: 'italic',
+      color: '#6e6e6e',
+      paddingLeft:10
+    },
+    input:{
+      fontSize:22,
+      paddingLeft:10
+    },
+    button: {
+      height:35,
+      width: 200,
+      backgroundColor: '#00aaff',
+      borderRadius: 10,
+      marginLeft:10
+    },
+    activetext: {
+      marginTop:4,
+      textAlignVertical:'center',
+      textAlign:'center',
+      color: '#ffffff',
+      fontSize:20,
+      paddingLeft:10
+    }
+})
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
@@ -63,22 +94,24 @@ export default class Login extends React.Component {
 
   render() {
     return (
-      <View style={{fontSize: 16}}>
-        <Text style={{marginTop: 50}}>Log In with email and password</Text>
-        <Text>Username:</Text>
-        <TextInput autoCapitalize="none" placeholder="username" onChangeText={(text) => this.setState({ loginUsername: text })}/>
+      <View >
+      <View style={{height:100, backgroundColor: '#00aaff'}}>
+        <Text style={{marginTop: 50, fontSize: 25, fontWeight: 'bold', color: '#ffffff', textAlign:'center'}}>Log in</Text>
+      </View>
+        <Text style={styles.text}>Username:</Text>
+        <TextInput style={styles.input} autoCapitalize="none" placeholder="username" onChangeText={(text) => this.setState({ loginUsername: text })}/>
 
-        <Text>Password:</Text>
-        <TextInput autoCapitalize="none" placeholder="password" onChangeText={(text) => this.setState({ loginPassword: text })}/>
+        <Text style={styles.text}>Password:</Text>
+        <TextInput style={styles.input} autoCapitalize="none" placeholder="password" onChangeText={(text) => this.setState({ loginPassword: text })}/>
         {/* <TouchableHighlight onPress={() => this.props.navigation.navigate('Main')}> */}
-        <TouchableHighlight onPress={() => this.verifyUser(this.state.loginUsername, this.state.loginPassword)}>
-          <Text>Log In</Text>
+        <TouchableHighlight onPress={() => this.verifyUser(this.state.loginUsername, this.state.loginPassword)} style= {styles.button}>
+          <Text style={styles.activetext}>Log In</Text>
         </TouchableHighlight>
 
         <View>
-          <Text>Don't have an account?</Text>
-          <TouchableHighlight onPress={() => this.setModalVisible(!this.state.modalVisible)}>
-            <Text>Sign up!</Text>
+          <Text style={styles.smallText}>Don't have an account?</Text>
+          <TouchableHighlight onPress={() => this.setModalVisible(!this.state.modalVisible)} style= {styles.button}>
+            <Text  style={styles.activetext}>Sign up!</Text>
           </TouchableHighlight>
 
           <Modal
