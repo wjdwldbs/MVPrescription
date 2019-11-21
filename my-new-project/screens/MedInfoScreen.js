25ce31fe234fe35ce8d4fc20eb0e7d97b5f6f1c5
Late1nAutumn <forestpu@163.com>
2019-11-20 20:01:03 -0800

    implement db to modal

--- my-new-project/screens/MedInfoScreen.js
+++ my-new-project/screens/MedInfoScreen.js
@@ -6,14 +6,36 @@ export default class MedInfoScreen extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      show:true
+      show:true,
+      //load: false, // should I?
+
+      name:"",
+      generic:"",
+      note:"",
+      sEffect:""
     };
+  }
+  componentDidMount(){
+
+    // fatal props : drugName
+    
+    fetch('http://localhost:3000/drug/'+this.props.drugName)
+      .then((data)=>{
+        var obj=data.json()[0];
+        var temp={name:this.props.drugName};
+        if(obj){
+          temp.generic = obj.generic || "";
+          temp.note = obj.not || "";
+          temp.sEffect = obj.sideEffect || "";
+        }
+        this.setState(temp);
+      });
   }
   render(){
     return (
       <Modal animationType="slide"
         height={500} width={0.9}
-        modalTitle={<ModalTitle title={this.props.name||'this.props.name'}/>}
+        modalTitle={<ModalTitle title={this.state.name||''}/>}
         visible={this.state.show}
         swipeDirection={['up', 'down']}
         swipeThreshold={100}
@@ -26,14 +48,12 @@ export default class MedInfoScreen extends React.Component{
               <Image style={{width:120, height:120}}
                 source={{uri:'https://hsm.utimaco.com/wp-content/uploads/2017/09/Applications_Grey_RGB_Random_Number_Generation-300x300.png'}}/>
               <View style={{flex:1, flexDirection:'column', justifyContent:'center'}}>
-                <Text style={{fontSize:22, paddingLeft:5}}>{this.props.name||'this.props.name'}</Text>
-                <Text style={{fontSize:20, paddingLeft:5}}>{this.props.type||'this.props.type'}</Text>
+                <Text style={{fontSize:22, paddingLeft:5}}>{this.state.name||''}</Text>
+                <Text style={{fontSize:20, paddingLeft:5}}>{this.state.generic||''}</Text>
               </View>
             </View>
-            <Text style={{fontSize:18, borderBottomColor:'lightgrey', borderBottomWidth:1, paddingBottom:8}}>{this.props.direction||
-              'this.props.direction : this part should be the information from FDA indicating the usage and amount patient should take for this medicine'}</Text>
-            <Text style={{fontSize:18, paddingTop:8}}>{this.props.sEffect||
-              'this.props.sEffect : this part should render the information about what side effect will this medicine cause provided by FDA'}</Text>
+            <Text style={{fontSize:18, borderBottomColor:'lightgrey', borderBottomWidth:1, paddingBottom:8}}>{this.state.note||''}</Text>
+            <Text style={{fontSize:18, paddingTop:8}}>{this.state.sEffect||''}</Text>
           </ScrollView>
         </ModalContent>
       </Modal>
